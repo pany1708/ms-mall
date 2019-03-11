ea135a2a03bb7ecaf54df528e9b3444c863a6472
abcd <abcd@126.com>
2019-03-11 14:53:40 +0800

    订单对象

--- microservice-orderservice/src/main/java/com/daocloud/po/Order.java
+++ microservice-orderservice/src/main/java/com/daocloud/po/Order.java
@@ -1,35 +1,21 @@
 package com.daocloud.po;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.ToString;
+
 import java.util.Date;
-
+@ApiModel(value="订单对象",description="订单对象")
+@Data
+@ToString
 public class Order {
+    @ApiModelProperty(value="主键",name="id",example="1",hidden = true)
     private Integer id;
+    @ApiModelProperty(value="编号",name="number",example="10002")
     private String number;
+    @ApiModelProperty(value="用户编号",name="userid",example="1")
     private Integer userid;
+    @ApiModelProperty(value="创建时间",name="time",hidden = true)
     private Date createtime;
-
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getNumber() {
-        return number;
-    }
-    public void setNumber(String number) {
-        this.number = number;
-    }
-    public Integer getUserid() {
-        return userid;
-    }
-    public void setUserid(Integer userid) {
-        this.userid = userid;
-    }
-    public Date getCreatetime() {
-        return createtime;
-    }
-    public void setCreatetime(Date createtime) {
-        this.createtime = createtime;
-    }
 }
