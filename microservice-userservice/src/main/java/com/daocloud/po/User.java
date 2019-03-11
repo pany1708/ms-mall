ca4c4650b4b89048f9b037b5bcf6fb2a931872be
abcd <abcd@126.com>
2019-03-11 14:52:16 +0800

    POJO

--- microservice-userservice/src/main/java/com/daocloud/po/User.java
+++ microservice-userservice/src/main/java/com/daocloud/po/User.java
@@ -1,27 +1,21 @@
 package com.daocloud.po;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.ToString;
+
 import java.io.Serializable;
 
+@Data
+@ToString
+@ApiModel(value="订单对象",description="订单对象")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
+	@ApiModelProperty(value="主键",name="id",example="1",hidden = true)
 	private Integer id;
+	@ApiModelProperty(value="主键",name="username",example="james")
 	private String username;
+	@ApiModelProperty(value="主键",name="address",example="address")
 	private String address;
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+
 }
