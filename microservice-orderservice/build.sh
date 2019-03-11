7b058437add5eba18955ca2a4b50937bde73b42d
abcd <abcd@126.com>
2019-03-11 14:54:08 +0800

    日志

--- microservice-orderservice/build.sh
+++ microservice-orderservice/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-scp target/microservice-orderservice-0.0.1-SNAPSHOT.jar root@192.168.0.99:~/orderservice/
-scp Dockerfile root@192.168.0.99:~/orderservice/
-ssh root@192.168.0.99
-cd ~/orderservice/
-mvn package
-docker build -t order-service:latest .
+mvn clean install
+VERSION=$(date +%Y%m%d%H%M%S)
+rm -rf *.zip
+zip -r order-service""$VERSION.zip ./*
