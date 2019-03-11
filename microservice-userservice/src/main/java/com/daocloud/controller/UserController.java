3fcf31293256c7a182af276dcad8377aac4e5a68
abcd <abcd@126.com>
2019-03-11 15:01:28 +0800

    默认

--- microservice-userservice/src/main/java/com/daocloud/controller/UserController.java
+++ microservice-userservice/src/main/java/com/daocloud/controller/UserController.java
@@ -3,6 +3,7 @@ package com.daocloud.controller;
 import com.daocloud.mapper.UserMapper;
 import com.daocloud.po.Order;
 import com.daocloud.po.User;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-
+@Api
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -30,9 +31,12 @@ public class UserController {
 	@Value("${ORDERSERVICEURL}")
 	private String ORDERSERVICEURL;
 
+	@ApiOperation(value = "获取用户订单信息", notes = "")
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 401, message = "Unauthorized"), @ApiResponse(code = 403, message = "Forbidden"),
+			@ApiResponse(code = 404, message = "Not Found"), @ApiResponse(code = 500, message = "Failure")})
 	@GetMapping(path="/findOrders/{username}")
-	public List<Order> getOrderByUsername(@PathVariable("username")
-												String username) {
+	public List<Order> getOrderByUsername(@ApiParam(value = "用户名") @PathVariable("username")  String username) {
 		LOG.info("username:"+username);
 		User user = this.userMapper.selectUser(username);
 		LOG.error("user:"+user);
